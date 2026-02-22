76015847569fd62997a82418966b4b5de716674b
erkanturk <refres98@gmail.com>
2026-02-22 12:44:30 +0300

    Dapper

---
+++ NetWebExamples/18_Dapper/Data/DapperContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace _18_Dapper.Data
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        //IDbConnection interface i ile lambda kullanarak bir sql yapısını method olarak kullanmayı sağladık.
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ NetWebExamples/18_Dapper/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace _18_Dapper.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }//Navigator  property
+        public Category Category { get; set; }//Foreignkey referansı
+    }
+}
